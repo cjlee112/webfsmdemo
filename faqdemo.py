316870820301f1ea4b38c36b1d3d9d1446a830d7
Christopher Lee <c@remail.zone>
2020-11-26 00:18:31 -0800

    many small fixes to chat messages, and requst status consistently

--- faqdemo.py
+++ faqdemo.py
@@ -19,13 +19,17 @@ async def main(threadURL='faqdemo.json', insertBeforeID=None):
         faqs = chat.filter_by_kind(mainThread, 'faq')
     await show_explanations(chat.filter_by_kind(mainThread, 'intro'), insertBeforeID)
     s = await pose_orct(chat.filter_by_kind(mainThread, 'orct')[0], insertBeforeID)
+    status = 0
     if s != 'same':
         if errmods:
-            await offer_errmods(errmods, insertBeforeID)
+            status += await offer_errmods(errmods, insertBeforeID)
         if faqs:
-            await offer_faqs(faqs, insertBeforeID)
+            status += await offer_faqs(faqs, insertBeforeID)
+        if status == 0:
+            await get_status((("ChatMessageTemplate", "How well do you feel you understand now? If you need more clarification, tell us."),),
+                            insertBeforeID)
         await solicit_faq(insertBeforeID)
-    await chat.continue_button((("ChatMessageTemplate", "Now you can move to the next lesson"),),
+    await chat.continue_button((("ChatMessageTemplate", "You've completed this question!  Let's continue to the next lesson."),),
                        insertBeforeID=insertBeforeID)
 
 async def show_explanations(explanations, insertBeforeID=None):
@@ -55,26 +59,20 @@ async def offer_errmods(errmods, insertBeforeID=None):
     ems = await chat.MultiSelection(chats, errmods, "em-list-template", "em-choice-template", insertBeforeID=insertBeforeID).get()
     for i in ems:
         await show_em(errmods[i], insertBeforeID)
-    chat.post_messages((("ChatMessageTemplate", "OK, let's continue."),),
-                       insertBeforeID=insertBeforeID)
-
-async def show_em_nostatus(em, insertBeforeID=None):
-    'show error model and get student status'
-    status = await chat.ChatQuery(((em, None), ("ChatMessageTemplate", "Hope you've overcame the misconception"),
-                         ("ChatMessageTemplate", "How well do you feel you understand this blindspot now? If you need more clarifications, tell us.")),
-                         'status-options-template', insertBeforeID=insertBeforeID).get()
-    if status == 'help':
-        chat.post_messages((("ChatMessageTemplate", "We will try to provide more explanation for this."),), insertBeforeID=insertBeforeID)
+    return len(ems)
 
 
 async def show_em(em, insertBeforeID=None):
     'show error model and get student status'
-    status = await chat.ChatQuery((('faq-a1', f'<b>RE: {em["title"]}</b><br>{em["message"]}'),
-                         ("ChatMessageTemplate", "Hope you've overcame the misconception"),
-                         ("ChatMessageTemplate", "How well do you feel you understand this blindspot now? If you need more clarifications, tell us.")),
-                         'status-options-template', insertBeforeID=insertBeforeID).get()
+    return await get_status((('faq-a1', f'<b>RE: {em["title"]}</b><br>{em["message"]}'),
+                         ("ChatMessageTemplate", "We hope this explanation helped you.  How well do you feel you understand this blindspot now? If you need more clarifications, tell us.")),
+                            insertBeforeID)
+
+async def get_status(chats, insertBeforeID=None, messageStamp=''):
+    status = await chat.ChatQuery(chats, 'status-options-template', insertBeforeID=insertBeforeID).get()
     if status == 'help':
-        chat.post_messages((("ChatMessageTemplate", "We will try to provide more explanation for this."),), insertBeforeID=insertBeforeID)
+        chat.post_messages((("ChatMessageTemplate", f"We will try to provide more explanation for this. {messageStamp}"),),
+                           insertBeforeID=insertBeforeID)
 
 
 
@@ -82,38 +80,38 @@ async def offer_faqs(faqlist, insertBeforeID=None):
     'let student select FAQs to view or create a new FAQ'
     chats = (("ChatMessageTemplate", "Would any of the following questions help you? Select the one(s) you wish to view."),)
     faqs = await chat.MultiSelection(chats, faqlist, "faq-list-template", "faq-choice-template", insertBeforeID=insertBeforeID).get()
+    status = 0
     for i in faqs:
-        await show_faq(faqlist[i], insertBeforeID)
+        status += await show_faq(faqlist[i], insertBeforeID)
+    return status
 
 async def solicit_faq(insertBeforeID=None):
     hasQuestion = await chat.ChatQuery((("ChatMessageTemplate", "Is there anything else you're wondering about, where you'd like clarification or something you're unsure about this point?"),),
                         'yesno-template', insertBeforeID=insertBeforeID).get()
     if hasQuestion == 'yes':
         await create_new_faq(insertBeforeID)
-    chat.post_messages((("ChatMessageTemplate", "OK, let's continue."),),
-                       insertBeforeID=insertBeforeID)
 
 
 async def show_faq(faq, insertBeforeID=None):
     'show full question and option to view answer'
-    helpful = await chat.ChatQuery((("faq-q1", f'<b>{faq["title"]}</b><br>{faq["question"]}'),
-                     ("ChatMessageTemplate", "Would the answer to this question help you?")), 'yesno-template', insertBeforeID=insertBeforeID).get()
+    helpful = await chat.ChatQuery((("FaqMessageTemplate", f'<b>{faq["title"]}</b><br>{faq["question"]}'),
+                     ("ChatMessageTemplate", "Would the answer to this student question help you?")), 'yesno-template', insertBeforeID=insertBeforeID).get()
     if helpful == 'yes' and 'answer' in faq:
         await show_faq_answer(faq['answer'], insertBeforeID)
+        return 1
+    return 0
 
 
 async def show_faq_answer(answer, insertBeforeID=None):
     'show answer and ask status'
-    status = await chat.ChatQuery((('faq-a1', answer), ("ChatMessageTemplate", "How well do you feel you understand now? If you need more clarification, tell us.")),
-                            "status-options-template", insertBeforeID=insertBeforeID).get()
-    if status == 'help':
-        chat.post_messages((("ChatMessageTemplate", "We will try to provide more explanation for this."),), insertBeforeID=insertBeforeID)
+    return await get_status((('faq-a1', answer), ("ChatMessageTemplate", "How well do you feel you understand now? If you need more clarification, tell us.")),
+                            insertBeforeID)
 
 
 async def create_new_faq(insertBeforeID=None, messageStamp=''):
     'student writes a new FAQ question'
-    title = await chat.ChatInput((("ChatMessageTemplate", f"First, write a 'headline version' of your question as a single sentence, as clearly and simply as you can. (You'll have a chance to explain your question fully in the next step). {messageStamp}"),), insertBeforeID=insertBeforeID).get()
-    text = await chat.ChatInput((("ChatMessageTemplate", f"Next, let's nail down exactly what you're unsure about, by applying your question to a real-world situation, to identify what specific outcome you're unsure about (e.g. 'is A going to happen, or B?') {messageStamp}"),), insertBeforeID=insertBeforeID).get()
+    title = await chat.ChatInput((("ChatMessageTemplate", f"First, write your question as a single sentence, as clearly and simply as you can. (You'll have a chance to explain your question fully in the next step). {messageStamp}"),), insertBeforeID=insertBeforeID).get()
+    text = await chat.ChatInput((("ChatMessageTemplate", f"Next, try saying a bit more about exactly what you're unsure of, such as a simple example where you can think of two possible answers but you're not sure which one is actually right. {messageStamp}"),), insertBeforeID=insertBeforeID).get()
     chat.post_messages((("ChatMessageTemplate", f"We'll try to get you an answer to this. {messageStamp}"),),
                        insertBeforeID=insertBeforeID)
 
@@ -133,11 +131,12 @@ async def offer_updates(updateFunc, updates, insertBeforeID, **kwargs):
 async def update_test(updates, insertBeforeID):
     'a trivial test of display a simple'
     faq = chat.filter_by_kind(updates, 'faq')[0]
-    await chat.continue_button((("ChatMessageTemplate", 'There are new answers for FAQs you are interested in <span class="chat-new-msg">new</span>'), ("ChatMessageTemplate", f'<b>{faq["title"]}</b><br>{faq["question"]} <span class="chat-new-msg">new</span>'),),
-                       insertBeforeID=insertBeforeID)
-    await chat.continue_button((("ChatMessageTemplate", f'{faq["answer"]} <span class="chat-new-msg">new</span>'),),
-                       insertBeforeID=insertBeforeID)
-    hasQuestion = await chat.ChatQuery((("ChatMessageTemplate", 'Have you anything else you are worried about? <span class="chat-new-msg">new</span>'),),
+    await get_status((("ChatMessageTemplate", '''Below is my answer to the question you raised RE: the following issue <span class="chat-new-msg">new</span>'''),    
+                        ("FaqMessageTemplate", f'<b>{faq["title"]}</b><br>{faq["question"]}'),
+                        ("ChatMessageTemplate", f'{faq["answer"]} <span class="chat-new-msg">new</span>'),
+                        ("ChatMessageTemplate", 'How well do you feel you understand now? If you need more clarification, tell us. <span class="chat-new-msg">new</span>')),
+                       insertBeforeID, '<span class="chat-new-msg">new</span>')
+    hasQuestion = await chat.ChatQuery((("ChatMessageTemplate", '''Is there anything else you're wondering about, where you'd like clarification or something you're unsure about this point? <span class="chat-new-msg">new</span>'''),),
                         'yesno-template', insertBeforeID=insertBeforeID).get()
     if hasQuestion == 'yes':
         await create_new_faq(insertBeforeID, messageStamp='<span class="chat-new-msg">new</span>')
